58b8549dacd72a3fdb43c88c2d412095e0b8cc19
Sneha De <55897319+sneha-afk@users.noreply.github.com>
2025-09-16 17:18:56 -0700

    fix: repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/new-site", // GitHub repo name
+  base: "/", // GitHub repo name
   plugins: [react()],
 });
